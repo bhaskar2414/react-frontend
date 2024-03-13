bfb720d78f2d560cb14acd07d9f2b516d1f54476
bhaskar2414 <bhaskar2414@hotmail.com>
2024-03-13 09:47:40 +0530

    Button text change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,20 +3,31 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 
 function Header() {
-    return (
-        <Box component="span">
-        <Button variant="outlined" href="/" sx={{ mt: 2, mr: 2, float: "right",textTransform: 'capitalize' }}>
-          Home
-        </Button>
-        <Button variant="outlined" href="/list" sx={{ mt: 2, mr: 2, float: "right",textTransform: 'capitalize' }}>
-          User List
-        </Button>
-        <Button variant="outlined" href="/login" sx={{ mt: 2, mr: 2, float: "right",textTransform: 'capitalize' }}>
-          Login
-        </Button>
-        
-      </Box>
-    )
+  return (
+    <Box component="span">
+      <Button
+        variant="outlined"
+        href="/"
+        sx={{ mt: 2, mr: 2, float: 'right', textTransform: 'capitalize' }}
+      >
+        Home
+      </Button>
+      <Button
+        variant="outlined"
+        href="/list"
+        sx={{ mt: 2, mr: 2, float: 'right', textTransform: 'capitalize' }}
+      >
+        Books List
+      </Button>
+      <Button
+        variant="outlined"
+        href="/login"
+        sx={{ mt: 2, mr: 2, float: 'right', textTransform: 'capitalize' }}
+      >
+        Login
+      </Button>
+    </Box>
+  );
 }
 
 export default Header;
